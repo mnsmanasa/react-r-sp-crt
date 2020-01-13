8470119f66323a0af96aa6ec12470bdb418da5d5
mnsmanasa <saimanasa1502@gmail.com>
2020-01-13 10:49:48 +0530

    1 file modified

--- store/reducer.js
+++ store/reducer.js
@@ -1,39 +1,48 @@
-import { GET_CARTLIST, ADD_TO_CART } from './actionTypes';
+import { GET_CARTLIST, ADD_TO_CART } from "./actionTypes";
 
 export const initialState = {
   cartList: [
     {
-      id: '1',
-      itemName: 'A',
-      description: 'Description of 1',
-      thumbnail:'https://via.placeholder.com/150/602b9e',
+      id: "1",
+      itemName: "A",
+      description: "Description of 1",
+      thumbnail: "https://via.placeholder.com/150/602b9e",
+      count: 0
     },
     {
-      id: '2',
-      itemName: 'B',
-      description: 'Description of 2',
-      thumbnail:'https://via.placeholder.com/150/602b9e',
-    },{
-      id: '3',
-      itemName: 'C',
-      description: 'Description of 3',
-      thumbnail:'https://via.placeholder.com/150/602b9e',
-    },{
-      id: '4',
-      itemName: 'D',
-      description: 'Description of 4',
-      thumbnail:'https://via.placeholder.com/150/602b9e',
-    },{
-      id: '5',
-      itemName: 'E',
-      description: 'Description of 5',
-      thumbnail:'https://via.placeholder.com/150/602b9e',
-    },{
-      id: '6',
-      itemName: 'F',
-      description: 'Description of 6',
-      thumbnail:'https://via.placeholder.com/150/602b9e',
+      id: "2",
+      itemName: "B",
+      description: "Description of 2",
+      thumbnail: "https://via.placeholder.com/150/602b9e",
+      count: 0
     },
+    {
+      id: "3",
+      itemName: "C",
+      description: "Description of 3",
+      thumbnail: "https://via.placeholder.com/150/602b9e"
+    },
+    {
+      id: "4",
+      itemName: "D",
+      description: "Description of 4",
+      thumbnail: "https://via.placeholder.com/150/602b9e",
+      count: 0
+    },
+    {
+      id: "5",
+      itemName: "E",
+      description: "Description of 5",
+      thumbnail: "https://via.placeholder.com/150/602b9e",
+      count: 0
+    },
+    {
+      id: "6",
+      itemName: "F",
+      description: "Description of 6",
+      thumbnail: "https://via.placeholder.com/150/602b9e",
+      count: 0
+    }
   ],
   addedToCart: []
 };
@@ -41,15 +50,16 @@ export const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_CARTLIST:
-      return state; 
+      return state;
     case ADD_TO_CART:
-    console.log(state.addedToCart)
+      console.log(state.addedToCart);
       return {
         ...state,
         addedToCart: [
-          ...state.addedToCart,  {...action.payload, noOfItems: action.noOfItems}
+          ...state.addedToCart,
+          { ...action.payload, noOfItems: action.noOfItems }
         ]
-      };   
+      };
     default:
       return state;
   }
